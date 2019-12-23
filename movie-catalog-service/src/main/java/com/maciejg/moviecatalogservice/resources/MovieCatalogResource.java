9bd9d55de723b18e997f9a7d54ddd35b74e2d508
m4ciejg <dragonek1996>
2019-12-23 22:07:24 +0100

    Eureka CLient added

--- movie-catalog-service/src/main/java/com/maciejg/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/maciejg/moviecatalogservice/resources/MovieCatalogResource.java
@@ -5,6 +5,7 @@ import com.maciejg.moviecatalogservice.models.Movie;
 import com.maciejg.moviecatalogservice.models.Rating;
 import com.maciejg.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,11 +32,11 @@ public class MovieCatalogResource {
     public List<CatalogItem> getCatalog(@PathVariable String userId) {
 
         //get all rated movie Ids
-        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
+        UserRating ratings = restTemplate.getForObject("http://rating-data-service/ratingsdata/users/" + userId, UserRating.class);
 
         return ratings.getUserReting().stream().map(rating -> {
            //For each movie Id, call movie info service and get details
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
             // Put them all together
             return new CatalogItem(movie.getName(), "Desc", rating.getRating());
         })
